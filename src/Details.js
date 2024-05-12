68258031663fae5ff11b4e5a19ec473ceee35aa6
vyankateshmisar <venkymisar@gmail.com>
2024-05-12 13:01:36 -0400

    First Commit

---
+++ src/Details.js
@@ -0,0 +1,176 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo3.png";
+import logo from "./assets/logo3.png";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Project Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Vyankatesh Misar",
+  tagline: "I am a ML Enthusiast and Software Developer",
+  img: profile,
+  about: `Hi, I am Vyankatesh! I am a highly ambitious and diligent Computer Science student at the University of Florida, seeking full time opportunities from May 2024 to augment my abilities and acquire new technologies. `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkedin: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Machine Learning Intern",
+    Company: `Neurotechnology Lab - Norman Fixel Institute for Neurological Disorders`,
+    Location: "Gainesville, FL",
+    Type: "Internship",
+    Duration: "April 2023 - Present",
+  },
+  {
+    Position: "Web Developer Intern",
+    Company: `University of Florida - Center for Undergraduate Research`,
+    Location: "Gainesville, FL",
+    Type: "Internship",
+    Duration: "January 2023 - April 2023",
+  },
+  {
+    Position: "Software Developer Intern",
+    Company: `Hummingbird Web Solutions Pvt. Ltd.`,
+    Location: "Pune, India",
+    Type: "Internship",
+    Duration: "January 2022 - May 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "University of Florida",
+    Company: "Master of Science, Computer Science",
+    Location: "Gainesville, FL",
+    Type: "GPA - 3.76/4.00",
+    Duration: "August 2022 - May 2024",
+  },
+  {
+    Position: "Savitribai Phule Pune University",
+    Company: `Bachelor of Engineering, Computer Engineering with Honors in Data Science`,
+    Location: "Pune, India",
+    Type: "GPA - 9.22/10.00",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "misarvyankatesh@gmail.com",
+  phone: "+1 (352) 721-1370",
+};
